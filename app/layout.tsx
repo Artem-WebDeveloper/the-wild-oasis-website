06e457d59249fa32f7d4015868fc1ef94d4341e0
Artem-WebDeveloper <ave_art@icloud.com>
2026-05-26 15:11:17 +0300

    feat: style home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,38 @@
-import React from 'react';
-import Navigation from '@/app/_components/Navigation';
-import Logo from '@/app/_components/Logo';
+import React from "react";
+
+import "@/app/_styles/globals.css";
+
+import { Josefin_Sans } from "next/font/google";
+import Header from "@/app/_components/Header";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
-  title: 'The Wild Oasis',
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beatiful mountains and dark forests",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+      <body
+        className={`${josefin.className} relative flex min-h-screen flex-col bg-primary-950 text-primary-100 antialiased`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="mx-auto max-w-7xl">{children}</main>
+        </div>
       </body>
     </html>
   );
